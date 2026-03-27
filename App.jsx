099b488bcde0d5d05be4273f64665dfff9f854d4
elbek <elbeksaparbayev05@gmail.com>
2026-03-27 10:11:08 +0500

    Added missing files for assignment

--- App.jsx
+++ App.jsx
@@ -1,15 +1,1 @@
-import React from 'react';
-import './App.css';
-import AboutUs from './components/AboutUs';
-
-function App() {
-    return (
-        <div className='background-image'>
-            <h1>Welcome to Paradise Nursery</h1>
-            <button>Get Started</button>
-            <AboutUs />
-        </div>
-    );
-}
-
-export default App;
+// Add your landing page JSX here
