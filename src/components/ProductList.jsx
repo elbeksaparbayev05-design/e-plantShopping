92fe986ddc09736af9da852b704de40f37791702
elbek <elbeksaparbayev05@gmail.com>
2026-03-27 09:45:00 +0500

    Add ProductList.jsx with plant categories and Add to Cart

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ProductList() {
+    return (
+        <div>
+            <h2>Houseplants</h2>
+            {/* Show at least 6 plants per category */}
+            {/* Add to Cart buttons */}
+        </div>
+    );
+}
+
+export default ProductList;
