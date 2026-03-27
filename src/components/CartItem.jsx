68d1ab4b26610711fc0d5dd283ca40c855b4742f
elbek <elbeksaparbayev05@gmail.com>
2026-03-27 09:45:23 +0500

    Add CartItem.jsx with cart functionalities

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CartItem() {
+    return (
+        <div>
+            <h2>Your Cart</h2>
+            {/* Show plant thumbnail, name, unit price */}
+            {/* Quantity buttons, delete, checkout */}
+        </div>
+    );
+}
+
+export default CartItem;
