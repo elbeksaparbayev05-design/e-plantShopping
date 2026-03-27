2811f2e24cc5c7b16ec30b927648e9f0c2e993c8
elbek <elbeksaparbayev05@gmail.com>
2026-03-27 09:43:38 +0500

    Add AboutUs.jsx file

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+    return (
+        <div>
+            <h1>Paradise Nursery</h1>
+            <p>Our mission is to provide the best houseplants...</p>
+        </div>
+    );
+}
+
+export default AboutUs;
